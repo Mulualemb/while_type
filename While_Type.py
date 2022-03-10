4f7b1f93087a011e8cd85740ee418273c8055c6e
Mulualemb <elibogala@gmail.com>
2022-03-10 09:30:21 +0200

    second commit

---
+++ While_Type.py
@@ -0,0 +1,28 @@
+#q1
+
+# n = 20
+# while n > 0:
+#     if n % 2 != 0:
+#         print(n)
+#     n = n - 1
+
+
+
+#q2 still bugged
+
+# question = input("do you want to pick even numbers or uneven numbers?:")
+# while question not in ("even","uneven"):
+#     print("please type 'even' or 'uneven' in the input box ")
+#     question = input("do you want to pick even numbers or uneven numbers:?")
+# if question == "even":
+#     even = int(input("type a number, you will get all the even numbers till 0:"))
+#     while even > 0:
+#         if even % 2 == 0:
+#             print(even)
+#         even = even - 1
+# elif question == "uneven":
+#     uneven = int(input("type a number,and you will get all the uneven numbers till 0:"))
+#     while uneven > 0:
+#         if uneven % 2 != 0:
+#             print(uneven)
+#         uneven = uneven - 1
